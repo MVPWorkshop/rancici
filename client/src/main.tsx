f614af04cd73870630921bf83d7f4c3cd06cfd3d
bojinovic <milos.bojinovic@mvpworkshop.co>
2024-02-09 16:17:57 +0100

    WIP: Game setup

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,45 +1,61 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { connect } from "starknetkit";
+import { Account } from "starknet";
+
 import App from "./App.tsx";
-import "./index.css";
+
 import { setup } from "./dojo/generated/setup.ts";
 import { DojoProvider } from "./dojo/DojoContext.tsx";
 import { dojoConfig } from "../dojoConfig.ts";
-import { connect } from "starknetkit"
-import { Account } from "starknet";
 
-async function init() {
-    const rootElement = document.getElementById("root");
-    if (!rootElement) throw new Error("React root not found");
-    const root = ReactDOM.createRoot(rootElement as HTMLElement);
+import "./style/index.css";
 
-    // Let the user pick a wallet (on button click)
-    const starknet = await connect()
+const DEV_BLOCK_WALLET_CONNECT = true;
 
-    if (!starknet) {
-    throw Error("User rejected wallet selection or silent connect found nothing")
-    }
+const init = async () => {
+  const rootElement = document.getElementById("root");
+  if (!rootElement) throw new Error("React root not found");
+  const root = ReactDOM.createRoot(rootElement as HTMLElement);
 
-    starknet.wallet?.enable()
-
-    let account: Account | undefined = undefined;
-    // Check if connection was successful
-    if(starknet.wallet?.isConnected) {
-        account = await starknet.wallet.account;
-    }
-
-    if(!account?.address) {
-        throw new Error("account not found")
-    }
-    const setupResult = await setup(dojoConfig(account));
-
+  if (DEV_BLOCK_WALLET_CONNECT == true) {
     root.render(
-        <React.StrictMode>
-            <DojoProvider value={setupResult}>
-                <App />
-            </DojoProvider>
-        </React.StrictMode>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
     );
-}
+    return;
+  }
+
+  // Let the user pick a wallet (on button click)
+  const starknet = await connect();
+
+  if (!starknet) {
+    throw Error(
+      "User rejected wallet selection or silent connect found nothing"
+    );
+  }
+
+  starknet.wallet?.enable();
+
+  let account: Account | undefined = undefined;
+  // Check if connection was successful
+  if (starknet.wallet?.isConnected) {
+    account = await starknet.wallet.account;
+  }
+
+  if (!account?.address) {
+    throw new Error("account not found");
+  }
+  const setupResult = await setup(dojoConfig(account));
+
+  root.render(
+    <React.StrictMode>
+      <DojoProvider value={setupResult}>
+        <App />
+      </DojoProvider>
+    </React.StrictMode>
+  );
+};
 
 init();
